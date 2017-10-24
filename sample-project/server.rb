7246da090b68bbb1f0dc6ae38c281056bd738707
Chrissy Fancy <chrissyfancy@gmail.com>
2017-10-23 21:32:48 -0400

    Create Sinatra app

---
+++ sample-project/server.rb
@@ -0,0 +1,13 @@
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/json"
+require "json"
+require "pry"
+
+get '/' do
+  send_file File.expand_path('index.html', settings.public_folder)
+end
+
+get '/users/:id' do
+  send_file File.expand_path('index.html', settings.public_folder)
+end
