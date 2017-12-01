0114a3014339005e0581f5058c6bb7a8abba50e6
Chrissy Fancy <chrissyfancy@gmail.com>
2017-12-01 12:10:15 -0500

    Refactor tests and add spies to test functions are called correctly

--- remote-math-service/test/test.js
+++ remote-math-service/test/test.js
@@ -1,14 +1,43 @@
-var Code = require("../src/code");
-var expect = require('chai').expect
+require('./SpecHelper')
 
-describe('code', () => {
-  describe('remoteMathService', () => {
-    it('should return the integer 3', () => {
-      let result = remoteMathService();
-      setTimeout(function () {
-        expect(result).to.be.equal(3);
-        done();
-      }, 1000);
-    });
-  })
-});
+describe('remoteMathService', () => {
+  it('should return the integer 3', (done) => {
+    remoteMathService(function(err, answer){
+      expect(answer).to.be.equal(3);
+      done();
+    })
+  });
+
+  it('should not return an undefined error', (done) => {
+    remoteMathService(function(err, answer){
+      expect(err).to.be.equal(undefined);
+      done();
+    })
+  });
+
+  it('should call the remoteMathService function once', (done) => {
+    let remoteMathServiceSpy = chai.spy();
+    remoteMathService(remoteMathServiceSpy)
+
+    expect(remoteMathServiceSpy).to.have.been.called.once;
+    done();
+  });
+
+  it('should call the callOneService function with the number 1', (done) => {
+    let callOneServiceSpy = chai.spy();
+    callOneService(callOneServiceSpy)
+
+    expect(callOneServiceSpy).to.have.been.called.with(1);
+    expect(callOneServiceSpy).to.not.have.been.called.with(2);
+    done();
+  });
+
+  it('should call the callTwoService function with the number 2', (done) => {
+    let callTwoServiceSpy = chai.spy();
+    callTwoService(callTwoServiceSpy)
+
+    expect(callTwoServiceSpy).to.have.been.called.with(2);
+    expect(callTwoServiceSpy).to.not.have.been.called.with(1);
+    done();
+  });
+})
