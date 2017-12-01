52242005768df773e1bde7a42331837635f1d911
Chrissy Fancy <chrissyfancy@gmail.com>
2017-12-01 12:08:46 -0500

    Refactor variables into spec helper

--- remote-math-service/test/SpecHelper.js
+++ remote-math-service/test/SpecHelper.js
@@ -1,3 +1,27 @@
+var code = require("../src/code");
+var chai = require('chai')
+var spies = require('chai-spies');
+var expect = chai.expect;
+var should = chai.should()
+
+var remoteMathService = code.remoteMathService
+var callOneService = code.callOneService
+var callTwoService = code.callTwoService
+
+Object.assign(global, {
+  code,
+  chai,
+  spies,
+  expect,
+  should,
+  remoteMathService,
+  callOneService,
+  callTwoService
+});
+
 beforeEach(() => {
   let _ = require('underscore')
+  chai.use(spies);
 });
+
+console.info(`TESTS RAN AT ${new Date().toLocaleTimeString()}`);
